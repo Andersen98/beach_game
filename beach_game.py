198b7d5c8c2dd655b763945de4e12aa336553357
ethana <thehumanme@gmail.com>
2017-07-28 10:58:35 -0400

    addid images and text folders for game

--- beach_game.py
+++ beach_game.py
@@ -1,6 +1,19 @@
+#Main library used for game engine
 import pygame
 
-from settings import settings
+#settings like screen size
+from settings import Settings
 
+#functions to be used during gameplay - i.e. check_snacks, check_radius
+import game_functions
+
+#Objects like map, room...functions like check_snacks, check_radius
+import game_unilities
+
+
+# initialize screen
+
+
+#run main game loop
 
 
